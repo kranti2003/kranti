25d7405290c3b5377940be4d35e98e20fb26682a
krantireddy2001 <kranti.reddy2001@gmail.com>
2023-03-02 12:28:21 +0530

    Array addition

---
+++ Python/python_numpy.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+arr1 = np.array ([[10, 20, 30], [40, 50, 60], [70, 80,90]])
+print ("array1 \n")
+print (arr1)
+
+arr2 = np.array ([[20, 30, 40], [50, 60, 70], [80, 90,100]])
+print ("array2 \n")
+print (arr2)
+arr3 = arr1 + arr2 
+print ("array3 \n")
+print (arr3)
